143ae868081abb4d87b294d427c86b84bd256667
Alam Wibowo <alamhafidz61@gmail.com>
2022-06-04 22:29:38 +0700

    add banner style

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -1,3 +1,5 @@
+import StandImage from './stand.png';
+import Image from 'next/Image';
 
 export default function Banner() {
     return (
@@ -8,6 +10,11 @@ export default function Banner() {
                     <p>Terbitkan Karya Terbaik Kamu Dan Tingkatan Penghasilan Bersama 1000+ Komunitas Publisher Kami Diseluruh Wilayah Indonesia.</p>
                 </div>
                 <button className="button-banner">Mendaftar Menjadi Penulis</button>
+            </div>
+            <div className="right-menu">
+                <div className="image-stand">
+                    <Image src={StandImage} alt="stand" />
+                </div>
             </div>
         </div>
     )
