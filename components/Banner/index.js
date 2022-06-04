124f52ad5976f405ea5adeca91097c09baf2d21e
Alam Wibowo <alamhafidz61@gmail.com>
2022-06-04 23:32:44 +0700

    Add Image Banner

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -1,4 +1,7 @@
 import StandImage from './stand.png';
+import Book1 from './images/book-1.png';
+import Book2 from './images/book-2.png';
+import Book3 from './images/book-3.png';
 import Image from 'next/Image';
 
 export default function Banner() {
@@ -14,6 +17,11 @@ export default function Banner() {
             <div className="right-menu">
                 <div className="image-stand">
                     <Image src={StandImage} alt="stand" />
+                </div>
+                <div className="book-display">
+                    <Image src={Book1} width={200} height={250}/>
+                    <Image src={Book2} width={200} height={250}/>
+                    <Image src={Book3} width={200} height={250}/>
                 </div>
             </div>
         </div>
