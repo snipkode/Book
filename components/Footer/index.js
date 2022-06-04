7f18ed8bd73ebd12b93c18839c9234287c9b7862
Alam Wibowo <alamhafidz61@gmail.com>
2022-06-04 20:20:56 +0700

    header stykcy and footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,6 +2,8 @@ import styles from './footer.module.css';
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>&copy; CV. Starlova Publishing Platform | All Right Reserved</div>
+    <footer>
+      <div className={styles.footer}>&copy; CV. Starlova Publishing Platform | All Right Reserved</div>
+    </footer>
   )
 }
