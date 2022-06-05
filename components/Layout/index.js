5204a1adf482d051617fd02779a37c255b8cb0fd
Alam Wibowo <alamhafidz61@gmail.com>
2022-06-05 21:02:16 +0700

    add google fonts

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -10,9 +10,12 @@ export default function Layout({ children, title }) {
                 <title>{title}</title>
                 <meta name="description" content="Starlova Publishing Platfrom" />
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet" />
             </Head>
             <Header />
-            <Banner/>
+            <Banner />
             <div className="main-bg">
                 {children}
             </div>
