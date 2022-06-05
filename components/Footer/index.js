f4237a4e3898684df170ae7cd2d7bf91cb319a64
Alam Wibowo <alamhafidz61@gmail.com>
2022-06-05 21:10:46 +0700

    remove footer sticky

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from './footer.module.css';
 export default function Footer() {
   return (
     <footer>
-      <div className={styles.footer}>&copy; CV. Starlova Publishing Platform | All Right Reserved</div>
+      <div className={styles.footer}>&copy; Copyright CV. Starlova Publishing Platform | All Right Reserved</div>
     </footer>
   )
 }
